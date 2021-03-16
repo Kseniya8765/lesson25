b53b432e1c3cd8a80c5bae4c14b50a371b58eb61
Kseniya8765 <tomashevich.kseniya@bk.ru>
2021-03-16 13:18:33 +0200

    add:test for func

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
-const dowbleElem =()=>{
-  const arr =[];
+const dowbleElem = (a,b) => {
+  const arr = [a,b];
   const arr2 = arr.map(el => el * 2);
   return arr2;
 }
---
+++ test.js
@@ -0,0 +1,9 @@
+describe("dowbleElem", function () {
+
+  it("удваивает каждый эл-т массива", () => {
+
+    assert.equal(dowbleElem(1, 2), ([2, 4]));
+
+  });
+
+});
