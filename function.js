e1b77c1d3960c3d5a6c789dbd562e60201f3d671
Kseniya8765 <tomashevich.kseniya@bk.ru>
2021-03-16 14:24:22 +0200

    add: conditions to func

--- function.js
+++ function.js
@@ -1,5 +1,18 @@
-const dowbleElem = (a,b) => {
-  let arr = [a,b];
+const dowbleElem = (a, b) => {
+  let arr = [a, b];
   let arr2 = arr.map(el => el * 2);
+
+  if (isNaN(a) || isNaN(b)) {
+    return 'error';
+  };
+
+  if (isNaN(a) && isNaN(b)) {
+    return 'error';
+  };
+
+  if (!Array.isArray(arr)){
+    return 'error';
+  };
+
   return arr2;
 }
--- test.js
+++ test.js
@@ -6,4 +6,21 @@ describe("dowbleElem", function () {
 
   });
 
+  it('возвращает текст "ошибка" если к-л аргумент не число', () => {
+
+    assert.deepEqual(dowbleElem(1, 'fbb'),'error');
+
+  });
+
+  it('возвращает текст "ошибка" если аргументы не числа', () => {
+
+    assert.deepEqual(dowbleElem('dfb', 'fbb'),'error');
+
+  });
+
+  it('возвращает текст "ошибка" если введен не массив', () => {
+
+    assert.equal(dowbleElem('dfb', 'fbb'),'error');
+
+	});
 });
